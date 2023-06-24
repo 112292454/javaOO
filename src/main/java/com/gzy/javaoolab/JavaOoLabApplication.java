ab647d3491597003b1b4d5dccb7df9e682c8577b
octopus <2841776039@qq.com>
2023-06-24 23:18:21 +0800

    enable face

--- src/main/java/com/gzy/javaoolab/JavaOoLabApplication.java
+++ src/main/java/com/gzy/javaoolab/JavaOoLabApplication.java
@@ -1,5 +1,6 @@
 package com.gzy.javaoolab;
 
+import com.gzy.javaoolab.utils.FaceModelInstance;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,7 +16,7 @@ public class JavaOoLabApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaOoLabApplication.class, args);
-//        FaceModelInstance.getInstance().init();
+        FaceModelInstance.getInstance().init();
     }
 
 }
--- src/main/java/com/gzy/javaoolab/config/AppConfig.java
+++ src/main/java/com/gzy/javaoolab/config/AppConfig.java
@@ -1,13 +1,13 @@
-//package com.gzy.javaoolab.config;
-//
-//import com.gzy.javaoolab.utils.FaceModelInstance;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class AppConfig {
-//    @Bean
-//    public FaceModelInstance faceModelInstance() {
-//        return FaceModelInstance.getInstance();
-//    }
-//}
+package com.gzy.javaoolab.config;
+
+import com.gzy.javaoolab.utils.FaceModelInstance;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public FaceModelInstance faceModelInstance() {
+        return FaceModelInstance.getInstance();
+    }
+}
